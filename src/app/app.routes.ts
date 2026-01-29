13f77a4e28d1c94db41517b7d6c208ebe53f089b
Adhinan S <adhinans@Adhinans-MacBook-Air-2.local>
2026-01-29 22:02:32 +0530

    Removed product & singleview modules and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { About } from './Ui/about/about';
 import { Service } from './Ui/service/service';
 import { Contact } from './Ui/contact/contact';
 import { Home } from './Ui/home/home';
-import { Product } from './Ui/product/product';
 import { Cyber } from './Ui/cyber/cyber';
 import { Devops } from './Ui/devops/devops';
 import { Gis } from './Ui/gis/gis';
 import { Data } from './Ui/data/data';
-import { Singleview } from './Ui/singleview/singleview';
 import { Cloud } from './Ui/cloud/cloud';
 
 export const routes: Routes = [
@@ -16,12 +14,10 @@ export const routes: Routes = [
   {path: 'services', component:Service},
   {path: 'contact', component:Contact },
   {path: 'home', component:Home},
-  {path: 'product', component:Product},
   {path: 'cyber', component:Cyber},
   {path: 'devops', component:Devops},
   {path: 'gis', component:Gis},
   {path: 'data', component:Data},
   {path: '', component:Home},
-  {path: 'singleview-list/:titleid', component:Singleview},
   {path: 'cloud', component:Cloud}
 ];
